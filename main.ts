e0e038397df527abe1e260856d6c8f3479304177
Huzaifa Qazi <160002762+huzaifaqazi@users.noreply.github.com>
2024-04-01 23:54:00 +0500

    number guessing game

---
+++ main.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from"inquirer"
+
+const RandomNumber=Math.floor(Math.random() * 4 +1 );
+
+const  answers =await inquirer.prompt([{
+    name:"userName",
+    type:"number",
+    message:"please a gussed number",
+
+}])
+if(answers.userName===RandomNumber){
+    console.log("congratulation,you guessd a rigth number")
+}else{
+    console.log("try again")
+}
